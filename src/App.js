011719a4424405d2de8852ac59ccf430e33dfe91
Karen919 <ka.arakelyan@mail.ru>
2023-04-14 23:01:33 +0400

    demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const demoUniqueNumberArr = [];
